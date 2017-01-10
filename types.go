a72c776ddedf6704c995133b220e9d2d304f5977
Rafał Krupiński <rkrupinski+github@gmail.com>
2017-01-10 16:04:38 +0000

    Add Payment

--- types.go
+++ types.go
@@ -39,5 +39,42 @@ type LedgerEntryEnvelope struct {
 
 const (
 	LedgerEntryPayment = "PAYMENT"
-	LedgerEntryRefund = "REFUND"
+	LedgerEntryRefund  = "REFUND"
 )
+
+type Payment struct {
+	PaymentId   int64 `json:"payment_id"`
+	ShopId      int32 `json:"shop_id"`
+	BuyerUserId int32 `json:"buyer_user_id"`
+
+	ReceiptId int64 `json:"receipt_id"`
+
+	AmountGross   int32 `json:"amount_gross"`
+	AmountFees    int32 `json:"amount_fees"`
+	AmountNet     int32 `json:"amount_net"`
+	PostedGross   int32 `json:"posted_gross"`
+	PostedFees    int32 `json:"posted_fees"`
+	PostedNet     int32 `json:"posted_net"`
+	AdjustedGross int32 `json:"adjusted_gross"`
+	AdjustedFees  int32 `json:"adjusted_fees"`
+	AdjustedNet   int32 `json:"adjusted_net"`
+
+	Currency      string `json:"currency"`
+	ShopCurrency  string `json:"shop_currency"`
+	BuyerCurrency string `json:"buyer_currency"`
+
+	ShippingUserId    int32 `json:"shipping_user_id"`
+	ShippingAddressId int32 `json:"shipping_address_id"`
+	BillingAddressId  int32 `json:"billing_address_id"`
+
+	Status string `json:"status"`
+
+	ShippedDate int32 `json:"shipped_date"`
+	CreateDate  int32 `json:"create_date"`
+	UpdateDate  int32 `json:"update_date"`
+}
+
+type PaymentEnvelope struct {
+	CommonEnvelope
+	Results []Payment `json:"results"`
+}
