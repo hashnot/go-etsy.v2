1d30c4cff3ece12107dc6d262eea823b21e7f998
Rafał Krupiński <rkrupinski+github@gmail.com>
2017-01-10 20:28:05 +0000

    Add Receipt

--- types.go
+++ types.go
@@ -74,7 +74,56 @@ type Payment struct {
 	UpdateDate  int32 `json:"update_date"`
 }
 
+type Receipt struct {
+	AdjustedGrandTotal int32         `json:"adjusted_grandtotal"`
+	BuyerEmail         string        `json:"buyer_email"`
+	BuyerUserId        int64         `json:"buyer_user_id"`
+	City               string        `json:"city"`
+	//ChannelBadgeSuffixString
+	CountryId          int64  `json:"country_id"`
+	CreationTime       int32  `json:"creation_tsz"`
+	CurrencyCode       string `json:"currency_code"`
+	DiscountAmount     int32  `json:"discount_amt"`
+	FirstLine          string `json:"first_line"`
+	GrandTotal         int32  `json:"grandtotal"`
+	// HasLocalDelivery
+	LastModifiedTime   int32  `json:"last_modified_tsz"`
+	MessageFromBuyer   string `json:"message_from_buyer"`
+	MessageFromPayment string `json:"message_from_payment"`
+	MessageFromSeller  string `json:"message_from_seller"`
+	Name               string `json:"name"`
+	//OrderId
+	PaymentEmail       string `json:"payment_email"`
+	PaymentMethod      string `json:"payment_method"`
+	ReceiptId          int64  `json:"receipt_id"`
+	//ReceiptType
+	SecondLine         string `json:"second_line"`
+	SellerEmail        string `json:"seller_email"`
+	SellerUserId       int64  `json:"seller_user_id"`
+	//ShowChannelBadge
+	//ShippingCarrier
+	//ShippingDetails
+	//ShippingNote
+	//ShippingNotificationDate
+	//ShippingTrackingCode
+	//Shipments
+	State              string `json:"state"`
+	Subtotal           int32  `json:"subtotal"`
+	TotalPrice         int32  `json:"total_price"`
+	TotalShippingCost  int32  `json:"total_shipping_cost"`
+	TotalTaxCost       int32  `json:"total_tax_cost"`
+	TotalVatCost       int32  `json:"total_vat_cost"`
+	//TransparentPriceMessage
+	WasPaid            bool   `json:"was_paid"`
+	WasShipped         bool   `json:"was_shipped"`
+	Zip                string `json:"zip"`
+
+	/**Shipment information associated to this receipt. */
+
+	//Coupon 	private 	transactions_r 	Coupon 	The coupon for the receipt.
+}
+
 type PaymentEnvelope struct {
 	CommonEnvelope
-	Results []Payment `json:"results"`
+	Results []Receipt `json:"results"`
 }
